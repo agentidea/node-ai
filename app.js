fe20cf3762b8cf180747fea21307fe726954c7f7
AgentIdea <oren.kredo@gmail.com>
2012-01-08 21:05:07 -0800

    rest of the non-default web app

---
+++ app.js
@@ -0,0 +1,55 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('view options', {
+    layout: false
+  });
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+//  app.use(express.cookieParser());
+//  app.use(express.session({ secret: 'your secret here' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+//
+//
+var users = [ 
+      { name: "foo", email: "foo@yahoo.com" }
+    , { name: "bar", email: "bar@bahoo.com" } 
+    , { name: "dee", email: "dee@bahoo.com" } 
+];
+
+
+// Routes
+
+app.get('/', routes.index);
+
+app.get('/users', function(req, res){
+  res.render('users', { users: users });
+});
+
+
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
